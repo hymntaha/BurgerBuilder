03e61a36e8bdc2d09535a08df44509f33594df85
Taha Uygun <tuygun@bluechipww.com>
2018-11-09 01:11:10 -0600

    adding price and ingredients menu items

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey=>{
         return [...Array(props.ingredients[igKey])].map((_,i)=>{
             return <BurgerIngredient key={igKey+i} type={igKey} />
@@ -17,7 +17,7 @@ const burger = (props) => {
     if (transformedIngredients.length === 0){
         transformedIngredients = <p>Please start adding ingredients!</p>
     }
-    
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
