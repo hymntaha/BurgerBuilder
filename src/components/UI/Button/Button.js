baa11cc50fcc79d05d8552a0115f8923db266439
Taha Uygun <tahauygun@gmail.com>
2018-11-10 18:34:35 -0600

    adding continue and cancel functionality in checkout

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,10 +3,11 @@ import classes from './Button.css';
 
 
 
-const Button = props => {
-   <button
-    className={[classes.Button, classes[props.btnType]].join(' ')}
-    onClick={props.clicked}>{props.children}</button>
-};
+const Button = (props) => (
+    <button
+        className={[classes.Button, classes[props.btnType]].join(' ')}
+        onClick={props.clicked}>{props.children}
+    </button>
+);
 
 export default Button;
