adbc56bb5025ce2cf9075a8dd1d255fbc4de5d99
Taha Uygun <tahauygun@gmail.com>
2018-11-11 23:10:25 -0600

    adding mobile view

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,9 @@ const Toolbar = props => {
     return (
         <header className={classes.Toolbar}>
             <div>MENU</div>
-            <Logo />
+            <div>
+                <Logo />
+            </div>            
             <nav>
                 <NavigationItems/>
             </nav>
