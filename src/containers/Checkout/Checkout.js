6135bcf9b06edff77f242592c87d42ae736a6d62
Taha Uygun <tahauygun@gmail.com>
2018-11-15 23:51:15 -0600

    checkout summary component integrated checkout

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+
+
+class Checkout extends Component {
+    render() {
+        return (
+            <div>
+                <CheckoutSummary ingredients={} />
+            </div>
+        );
+    }
+}
+
+export default Checkout;
