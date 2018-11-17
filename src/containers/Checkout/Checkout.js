fd1ffa5daa38ab2dfaf87a5a3e2814e22621e512
Taha Uygun <tahauygun@gmail.com>
2018-11-16 22:53:03 -0600

    using props history for checkout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -13,10 +13,18 @@ class Checkout extends Component {
         }
     }
 
+    checkoutCancelledHandler = () =>{
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () =>{
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render() {
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
+                <CheckoutSummary ingredients={this.state.ingredients} checkoutCancelled={} checkoutContinued={} />
             </div>
         );
     }
