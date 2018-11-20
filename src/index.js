971224f45f4174fac393f4ca3c7504cfdd60bde0
Taha Uygun <tahauygun@gmail.com>
2018-11-19 23:27:33 -0600

    passing provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+
+
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const app = (
+  <Provider>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render( app, document.getElementById( 'root' ) );
