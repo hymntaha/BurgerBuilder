8e2d34b671095170c9c435ae77a29067669a5176
Taha Uygun <tahauygun@gmail.com>
2018-11-21 23:25:03 -0600

    arrengements

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { createStore } from "redux";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import burgerBuilder from "./store/reducers/reducer";
+import burgerBuilder from "./store/reducers/burgerBuilder";
 
 const store = createStore(
   burgerBuilder, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
