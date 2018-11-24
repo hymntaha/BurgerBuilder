806e039606f2d9abfddb8540d223054ae51ef6fd
Taha Uygun <tahauygun@gmail.com>
2018-11-23 23:56:45 -0600

    UI updates

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -23,7 +23,7 @@ class Checkout extends Component {
     render () {
         let summary = <Redirect to="/"/>
         if (this.props.ings) {
-          const purchasedRedirect = <Redirect/>
+          const purchasedRedirect =  this.props.purchased ? <Redirect to="/" /> : null;
 
           summary = (
             <div>
@@ -43,7 +43,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 }
 
