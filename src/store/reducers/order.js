04eb22fe01d2757b28c8ca16f3abf31726dbdbfc
Taha Uygun <tahauygun@gmail.com>
2018-11-23 22:26:21 -0600

    adding loading action types

---
+++ src/store/reducers/order.js
@@ -0,0 +1,29 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+  orders: [],
+  loading: false
+}
+
+const reducer = (state = initialState,action ) => {
+  switch (action.type){
+    case (actionTypes.PURCHASE_BURGER_SUCCESS):
+      const newOrder = {
+      ...action.order,
+      id: action.orderId
+    }
+      return {
+        ...state,
+        loading: false,
+        orders: state.orders.concat()
+      };
+    case (actionTypes.PURCHASE_BURGER_FAIL):
+      return {
+        ...state,
+        loading: false
+      };
+    default: state;
+  }
+};
+
+export default reducer;
