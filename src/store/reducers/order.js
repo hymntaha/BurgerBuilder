4b9495f29216efe698ddf8af04ea8a01c96fe9c4
Taha Uygun <tahauygun@gmail.com>
2018-11-24 16:37:09 -0600

    fetch order via redux continues

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -35,7 +35,24 @@ const reducer = (state = initialState,action ) => {
         ...state,
         loading: false
       };
-    default: state;
+    case (actionTypes.FETCH_ORDERS_START):
+      return {
+        ...state,
+        loading:true
+      };
+    case (actionTypes.FETCH_ORDERS_SUCCESS):
+      return {
+        ...state,
+         order: action.orders,
+        loading: false
+      };
+    case (actionTypes.FETCH_ORDERS_FAIL):
+      return {
+        ...state,
+        loading: false
+      };
+    default:
+      return state;
   }
 };
 
