12d08cf817098f08608e478d85c9e0639dd384dc
Taha Uygun <tahauygun@gmail.com>
2018-11-24 16:20:47 -0600

    fetching orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -41,3 +41,29 @@ export const purchaseInit = () => {
     type: actionTypes.PURCHASE_INIT
   };
 };
+
+export const fetchOrdersSuccess = (orders) => {
+  return{
+    type:actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  };
+};
+
+export const fetchOrdersFail = (error) => {
+  return{
+    type:actionTypes.FETCH_ORDERS_FAIL,
+    orders: error
+  };
+};
+
+export const fetchOrdersStart = () =>{
+  return{
+    type: actionTypes.FETCH_ORDERS_START
+  };
+};
+
+export const fetchOrders = () => {
+  return dispatch => {
+
+  };
+};
