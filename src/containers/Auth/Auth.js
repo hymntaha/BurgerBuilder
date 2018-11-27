69cb0b1dd38667a1dbda20214af922424712c813
Taha Uygun <tahauygun@gmail.com>
2018-11-26 22:48:39 -0600

    redirect for order

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,7 +6,7 @@ import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import Spinner from "../../components/UI/Spinner/Spinner";
-
+import { Redirect } from "react-router-dom";
 
 
 class Auth extends Component {
@@ -132,8 +132,16 @@ class Auth extends Component {
         );
       }
 
+
+      let authRedirect = null;
+
+      if (this.props.isAuthenticated) {
+        authRedirect = <Redirect to="/"/>
+      }
+
         return (
             <div className={classes.Auth}>
+              {authRedirect}
               {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
@@ -150,7 +158,8 @@ class Auth extends Component {
 const mapStateToProps = state =>{
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null
     };
 };
 
