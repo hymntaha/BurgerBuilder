b926ad819b9950278a4be3ce0ca34f9d20499d53
Taha Uygun <tahauygun@gmail.com>
2018-11-27 22:58:46 -0600

    Persistent Auth State with localStorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -27,6 +27,7 @@ export const logout = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("expirationDate");
   localStorage.removeItem("userId");
+
   return {
     type: actionTypes.AUTH_LOGOUT
   }
