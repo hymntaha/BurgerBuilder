44c7783a5520e7cdb22546f5f8dd6e8f59cbfebb
Taha Uygun <tahauygun@gmail.com>
2018-11-27 22:58:39 -0600

    Persistent Auth State with localStorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -26,7 +26,7 @@ export const authFail = (error) => {
 export const logout = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("expirationDate");
-
+  localStorage.removeItem("userId");
   return {
     type: actionTypes.AUTH_LOGOUT
   }
@@ -58,18 +58,21 @@ export const auth = (email, password, isSignup) => {
         }
 
 
-        axios.post(url,authData)
-          .then(response=>{
-            console.log(response);
-            const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-            localStorage.setItem("token", response.data.idToken);
-            localStorage.setItem('expirationDate',expirationDate);
-            dispatch(authSuccess(response.data.idToken, response.data.localId));
-            dispatch(checkAuthTimeout(response.data.expiresIn));
-          })
-          .catch(err=> {
-            dispatch(authFail(err.response.data.error));
-          });
+      axios.post(url, authData)
+        .then(response => {
+          console.log(response);
+          const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+          localStorage.setItem("token", response.data.idToken);
+          localStorage.setItem("expirationDate", expirationDate);
+          localStorage.setItem("userId", response.data.localId);
+
+
+          dispatch(authSuccess(response.data.idToken, response.data.localId));
+          dispatch(checkAuthTimeout(response.data.expiresIn));
+        })
+        .catch(err => {
+          dispatch(authFail(err.response.data.error));
+        });
 
     };
 };
