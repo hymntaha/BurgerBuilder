ec23de1fd8cf90c927c6767a6c4bea6cc6d00710
Taha Uygun <tahauygun@gmail.com>
2018-11-27 23:08:46 -0600

    fixing connect and routing errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch,withRouter } from 'react-router-dom';
 import { connect } from "react-redux";
 
 import Layout from './hoc/Layout/Layout';
@@ -34,7 +34,9 @@ class App extends Component {
 }
 
 const mapDispatchToProps = dispatch => {
-  return onTryAutoSignup: () => dispatch(actions.authCheckState())
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
 }
 
-export default connect(null,mapDispatchToProps)(App);
+export default withRouter(connect(null,mapDispatchToProps)(App));
